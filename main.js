35ad2bc51d0627e39d0d1ae40c572f2c78ec68ee
saibal <lorenzo.forti@gmail.com>
2024-12-11 20:35:16 +0100

    - first mod

---
+++ main.js
@@ -0,0 +1,34 @@
+/**
+* @preserve
+* Filename: index.js
+*
+* Created: 11/12/2024 (19:55:25)
+* Created by: Lorenzo Saibal Forti <lorenzo.forti@gmail.com>
+*
+* Last update: 11/12/2024 (19:55:32)
+* Updated by: Lorenzo Saibal Forti <lorenzo.forti@gmail.com>
+*
+* Copyleft: 2024 - Tutti i diritti riservati
+*
+* Comments:
+*/
+
+const validUrl = require("valid-url")
+const validate = require("./lib/validate")
+
+module.exports = async (options) => {
+
+	if (!options) {
+		throw new Error("Missing required input: options object");
+	}
+
+	if (!options.url && !options.data) {
+		throw new Error("Missing required params: url or data");
+	}
+
+	if (options.url && !validUrl.isWebUri(options.url)) {
+		throw new Error("Invalid url");
+	}
+
+	return validate(options);
+}
